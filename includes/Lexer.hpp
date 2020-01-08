046c70f354e7ea5bbadfd8bae5857bcdcd1a3207
Emilien Baudet <emilien.baudet@gmail.com>
2020-01-08 19:56:29 +0100

    [dev] lexer BNF

--- includes/Lexer.hpp
+++ includes/Lexer.hpp
@@ -6,12 +6,44 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/09 19:27:25 by ebaudet           #+#    #+#             */
-/*   Updated: 2019/12/09 20:15:54 by ebaudet          ###   ########.fr       */
+/*   Updated: 2020/01/08 19:29:37 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #if !defined(LEXER_HPP)
 #define LEXER_HPP
+/*
+**
+BNF (Backus–Naur form) grammar :
+
+S := INSTR [SEP INSTR]* #
+
+INSTR :=
+	push VALUE
+	| pop
+	| dump
+	| assert VALUE
+	| add
+	| sub
+	| mul
+	| div
+	| mod
+	| print
+	| exit
+
+VALUE :=
+	int8(N)
+	| int16(N)
+	| int32(N)
+	| float(Z)
+	| double(Z)
+
+N := [-]?[0..9]+
+
+Z := [-]?[0..9]+.[0..9]+
+
+SEP := '\n'+
+*/
 
 class Lexer {
     public:
